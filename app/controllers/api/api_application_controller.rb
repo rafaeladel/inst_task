88ca6ecdbd78752fa192f19deceaad653d6f88d7
rafael <rafael.adel20@gmail.com>
2016-05-23 20:41:35 +0200

    init

--- app/controllers/api/api_application_controller.rb
+++ app/controllers/api/api_application_controller.rb
@@ -2,12 +2,12 @@ module Api
   class ApiApplicationController < ApplicationController
     protect_from_forgery with: :null_session
 
-    # rescue_from StandardError do |e|
-    #   render_rescue_json(e)
-    # end
-    #
-    # def render_rescue_json(exception)
-    #   render json: { error: "Oops. Something went wrong." }, status: 500
-    # end
+    rescue_from StandardError do |e|
+      render_rescue_json(e)
+    end
+
+    def render_rescue_json(exception)
+      render json: { error: exception.message }, status: 500
+    end
   end
 end
