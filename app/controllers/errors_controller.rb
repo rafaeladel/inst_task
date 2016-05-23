3b1528199701d23ea10d97fdefba17fb6586e5a9
rafael <rafael.adel20@gmail.com>
2016-05-23 21:27:32 +0200

    init

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,11 +1,7 @@
 class ErrorsController < ApplicationController
 
   def not_found
-    if env['REQUEST_PATH'] =~ /^\/api/
-      render json: {error: "404 not found"}, status: 404
-    else
-      render text: "404 not found", status: 404
-    end
+    render json: {error: "404 not found"}, status: 404
   end
 
 end
